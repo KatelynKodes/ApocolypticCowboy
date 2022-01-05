d75ce4b86558df645f5aab8c3201fafcc0498b94
Peyton <peytonsemail2000@gmail.com>
2022-01-05 08:50:06 -0600

    Changed enemy health

--- raygame/MainScene.cpp
+++ raygame/MainScene.cpp
@@ -13,7 +13,7 @@ void MainScene::start()
 	CircleCollider* playerCollider = new CircleCollider(25, player);
 	player->setCollider(playerCollider);
 
-	FollowEnemy* enemy = new FollowEnemy(20, 20, "enemy", 20, 100, player);
+	FollowEnemy* enemy = new FollowEnemy(20, 20, "enemy", 1000, 100, player);
 	enemy->getTransform()->setScale({ 50, 50 });
 
 	addActor(player);
